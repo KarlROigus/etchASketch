095d895ca36eca21d4c2700af1a4e8b11af86875
Karl Rudolf <karlrudolfoigus@gmail.com>
2024-06-22 11:51:51 +0300

    Added the feature to ask for grid side length.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-for (let i = 0; i < 6; i++) {
-    const bigContainer = document.querySelector(".container");
-    const oneRow = document.createElement("div");
-    oneRow.className = "big";
-    for (let i = 0; i < 6; i++) {
-        const someDiv = document.createElement("div");
-        someDiv.className = "small";
-        oneRow.appendChild(someDiv);
+function makeRequestedGrid() {
+    const gridSide = prompt("Enter the grid side length: ");
+    for (let i = 0; i < parseInt(gridSide); i++) {
+        const bigContainer = document.querySelector(".container");
+        const oneRow = document.createElement("div");
+        oneRow.className = "big";
+        for (let i = 0; i < parseInt(gridSide); i++) {
+            const someDiv = document.createElement("div");
+            someDiv.className = "small";
+            oneRow.appendChild(someDiv);
+        }
+        bigContainer.appendChild(oneRow);
     }
 
-    bigContainer.appendChild(oneRow);
 }
+
+
+makeRequestedGrid();
 
 
 
